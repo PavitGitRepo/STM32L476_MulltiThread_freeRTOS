a8e6f47a81fa9d5f441ba29f03482d680d9a79b5
Pavit Kumar <pavitb16@gmail.com>
2026-09-20 22:23:57 +0530

    ADXL345 x,y,z axis read and x,y angle reading

--- multithread_sensors_node/Core/Inc/adxl345i2c.h
+++ multithread_sensors_node/Core/Inc/adxl345i2c.h
@@ -1,9 +1,31 @@
 #ifndef __ADXL345I2C_H__
 #define __ADXL345I2C_H__
 
+#include <math.h>
+
+#define PI 3.141592654
 
 #define ADXL345_ADDR   (0x53 << 1)
 
-uint8_t get_devid(uint8_t addr);
+#define BW_RATE        0x2C         /*Data rate and power mode control*/
+#define INT_SOURCE     0x30         /*Source of interrupts*/
+#define POWER_CTL      0x2D         /*Power Control register*/
+#define DATA_FORMAT    0x31         /*axis data format register*/
+
+#define DATAX0 0x32 /*X-Axis Data 0*/
+#define DATAX1 0x33 /*X-Axis Data 1*/
+#define DATAY0 0x34 /*Y-Axis Data 0*/
+#define DATAY1 0x35 /*Y-Axis Data 1*/
+#define DATAZ0 0x36 /*Z-Axis Data 0*/
+#define DATAZ1 0x37 /*Z-Axis Data 1*/
+
+uint8_t i2c_read_devid(uint8_t addr);
+
+uint8_t i2c_read_register(uint8_t device_addr, uint8_t reg_to_read);
+uint8_t i2c_write_register(uint8_t device_addr, uint8_t reg_to_write, uint8_t val_to_write);
+
+uint16_t i2c_read_x_axis(void);
+uint16_t i2c_read_y_axis(void);
+uint16_t i2c_read_z_axis(void);
 
 #endif
